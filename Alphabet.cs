58050c5620a723267a390cfdfef76149934acf43
Lilit Khachatryan <lilit.a.khachatryan@praemium.com>
2021-12-07 23:16:03 +0400

    Add project files.

---
+++ Alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LanguageDetectorApp
+{
+    public class Alphabet
+    {
+        public static string ArmenianAlphabet = "Ա, Բ, Գ, Դ, Ե, Զ, Է, Ը, Թ, Ժ, Ի, Լ, Խ, Ծ, Կ, Հ, Ձ, Ղ, Ճ, Մ, Յ, Ն, Շ, Ո, Չ, Պ, Ջ, Ռ, Ս, Վ, Տ, Ր, Ց, Ւ, Փ, Ք, և, Օ, Ֆ";
+        public static string RussianAlphabet = "а, б, в, г, д, е, ё, ж, з, и, й, к, л, м, н, о, п, р, с, т, у, ф, х, ц, ч, ш, щ, ъ, ы, ь, э, ю, я";
+        public static string EnglishAlphabet = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LanguageDetectorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding unicode = Encoding.Unicode;
+            System.Console.InputEncoding = unicode;
+
+            Console.WriteLine("Please, write a text to detect a language:");
+            var text = Console.ReadLine();
+            Console.WriteLine();
+            if (text.Any(char.IsDigit))
+            {
+                Console.WriteLine("Enter only letters, please");
+                text = Console.ReadLine();
+            }
+
+            text = Regex.Replace(text, @"\s+", "");
+
+            var language = CheckLanguage(text);
+            Console.WriteLine(language);
+        }
+
+        static string CheckLanguage(string text)
+        {
+            var letters = text.ToUpper();
+            Dictionary<string, int> Numbers = new Dictionary<string, int>();
+            string language = "No language detected";
+
+            var countAM = 0;
+            var countEN = 0;
+            var countRU = 0;
+
+            foreach (var letter in letters)
+            {
+                if (Alphabet.ArmenianAlphabet.ToUpper().Contains(letter))
+                {
+                    countAM++;
+                }
+
+                else if (Alphabet.EnglishAlphabet.ToUpper().Contains(letter))
+                {
+                    countEN++;
+                }
+
+                else if (Alphabet.RussianAlphabet.ToUpper().Contains(letter))
+                {
+                    countRU++;
+                }
+
+            }
+
+            Numbers.Add("Armenian", countAM);
+            Numbers.Add("English", countEN);
+            Numbers.Add("Russian", countRU);
+
+            var max = Numbers.Values.ToList().Max();
+            if (max == letters.Length)
+                language = Numbers.Where(n => n.Value == max).First().Key;
+
+            return language;
+        }
+    }
+}
